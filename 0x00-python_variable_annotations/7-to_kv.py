f415845571f1301981fd6a6446d1c9637034f7fd
Lynnmich <lynnenmichuki@gmail.com>
2023-08-03 13:08:11 +0000

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Function that takes arguments and returns a tuple"""
+from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
-    """returnsa string and a float"""
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns a string and a float"""
     return k, v**2
