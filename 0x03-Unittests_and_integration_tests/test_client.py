6627f5df8e73a594fe11f9be157bac8e50691b19
Lynnmich <lynnenmichuki@gmail.com>
2023-08-28 16:46:52 +0300

    Task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,4 +4,15 @@ import unittest
 
 
 class TestGithubOrgClient(unittest.TestCase):
-
+    """TestGithubOrgClient Class"""
+    @patch("client.get_json")
+    @parameterized.expand([
+        "google",
+        "abc"
+    ])
+    def test_org(self, info, mock):
+        """New test"""
+        endpoint = 'https://api.github.com/orgs/{}'.format(info)
+        spec = GithubOrgClient(info)
+        spec.org()
+        mock.assert_called_once_with(endpoint)
